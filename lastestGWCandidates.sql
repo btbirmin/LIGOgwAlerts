ded0d851a03cdfceafabcf74311c27a4b1e700d7
btbirmin <32885426+btbirmin@users.noreply.github.com>
2023-04-27 16:25:32 -0700

    mySql Table

---
+++ lastestGWCandidates.sql
@@ -0,0 +1,11 @@
+CREATE TABLE latestGWCandidates(
+uid VARCHAR(255) PRIMARY KEY, 
+labels VARCHAR(999), 
+t_start VARCHAR(255),
+t_0 Varchar(255),
+t_end varchar(255),
+far_hz Varchar(255),
+createddate Varchar(255),
+insertdate datetime,
+acknowledged varchar(20)
+); 
